55c6d3ff0bb7dfef92e2c0021809dc9cf12e6c77
Abhimanyu Pathania <pathania.abhimanyu@gmail.com>
2021-01-23 01:25:18 +0530

    add router xstate and chakraui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ChakraProvider>
+        <Switch>
+          <Route component={LandingPage} />
+        </Switch>
+      </ChakraProvider>
+    </Router>
   );
 }
 
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,9 @@
+import Landing from '../components/Landing';
+
+export default function LandingPage() {
+  return (
+    <div>
+      <Landing />
+    </div>
+  );
+}
