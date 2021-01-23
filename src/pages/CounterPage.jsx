e3419c237282b9182c8cea964c528627315b7449
Abhimanyu Pathania <pathania.abhimanyu@gmail.com>
2021-01-24 00:44:16 +0530

    add counter gui

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,3 +1,57 @@
+import { useMachine } from '@xstate/react';
+import {
+  Box,
+  Container,
+  Button,
+  Stack,
+  Input,
+  FormControl,
+  FormLabel,
+  Heading,
+} from '@chakra-ui/react';
+
+import counterMachine, { counterActions } from '../machines/counter';
+
 export default function CounterPage() {
-  return <div>CounterPage</div>;
+  const [current, send] = useMachine(counterMachine);
+
+  return (
+    <Container centerContent>
+      <Heading>1. Counter</Heading>
+      <Box
+        padding="3"
+        mt="3"
+        borderColor="gray.200"
+        borderWidth="1px"
+        borderRadius="md">
+        <Box>
+          <FormControl id="email">
+            <FormLabel>Count</FormLabel>
+            <Input variant="filled" value={current.context.count} disabled />
+          </FormControl>
+        </Box>
+        <Stack
+          direction="row"
+          spacing={4}
+          align="center"
+          mt="1rem"
+          justify="center">
+          <Button
+            colorScheme="teal"
+            size="sm"
+            variant="solid"
+            onClick={() => send(counterActions.INCREMENT)}>
+            Increment
+          </Button>
+          <Button
+            colorScheme="teal"
+            size="sm"
+            variant="outline"
+            onClick={() => send(counterActions.RESET)}>
+            Reset
+          </Button>
+        </Stack>
+      </Box>
+    </Container>
+  );
 }
